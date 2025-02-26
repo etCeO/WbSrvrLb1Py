e536db5b694afc84880b1e49127a1073bb56b067
Ethan E Lopez <etlopez@chapman.edu>
2025-02-26 11:51:00 -0800

    Update WebServer.py

--- WebServer.py
+++ WebServer.py
@@ -2,12 +2,13 @@
 from socket import *
 import sys # In order to terminate the program
 
+serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
 # creates a server socket using internet protocols and http requests
 
 #Prepare a server socket
 
-serverSocket.bind(('', 8080))
+serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 
 while True:
@@ -17,7 +18,7 @@ while True:
     connectionSocket, addr = serverSocket.accept()
     # the server accepts incoming connections and returns the address
     try:
-        message = connectionSocket.recv(1024)
+        message = connectionSocket.recv(1024).decode()
         filename = message.split()[1]
         # parses http for filename in terminal at index 1 as a string
         f = open(filename[1:])
@@ -28,7 +29,6 @@ while True:
         #Send one HTTP header line into socket
 
         connectionSocket.send("HTTP/1.1 200 OK\r\n")
-        connectionSocket.send("Content-Type: text/html\r\n\r\n")
 
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
@@ -40,7 +40,6 @@ while True:
     except IOError:
         #Send response message for file not found
         connectionSocket.send("HTTP/1.1 404 Not Found\r\n")
-        connectionSocket.send("Content-Type: text/html\r\n\r\n")
         #Close client socket       
         connectionSocket.close() 
 
